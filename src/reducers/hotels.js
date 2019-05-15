e3f0b4d9ec81dc4e4057bc2183a65244df4367fd
mpatelashish <mpatelashish@gmail.com>
2019-05-15 22:46:55 +0530

    changes 1

--- src/reducers/hotels.js
+++ src/reducers/hotels.js
@@ -2,7 +2,6 @@ export default function hotels(state = [], action) {
   switch (action.type) {
     case 'HOTEL_LIST':
       return action.result.hotels;
-    // initial state
     default:
       return state;
   }
